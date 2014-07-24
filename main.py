51495496a45eaa2a25d9b87a14185979a89c92d9
Liang Shang <liang.shang13@imperial.ac.uk>
2014-07-24 13:55:16 +0100

    write <time command> to file so bash can run it

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
-from configuration import memories, cpus
+from learning.lxc_config import memories, cpus
 from learning.exe_command import run_command_with_docker, run_command
 from learning.exe_time import parse_time
-from table import PerformanceTable
+from learning.table import PerformanceTable
 
 __author__ = 'Sherlock'
 
@@ -10,18 +10,31 @@ if __name__ == '__main__':
 
     table = PerformanceTable()
 
+    if len(sys.argv) < 2:
+        print "Usage: python main.py <command>"
+        exit()
+
     command = sys.argv[1]
     image = "stackbrew/hipache"
 
+    f = open("script", "w")
+    f.write("time "+command)
+    f.close()
+
     for memory in memories:
         for cpu in cpus:
-            a, b = run_command_with_docker(" python exe_time.py "+command, image, cpu, memory)
+            print "begin to calculate execution time with config: "+\
+                  str(cpu)+" cpu(s) and "+str(memory)+"k memory"
+            a, b = run_command_with_docker("bash /Final/script", image, cpu, memory)
+            #a, b = run_command(" time ls")
             if a != 0:
                 print "command '"+command+"' is wrong"
                 print b
                 exit()
-            total_time = b
+            print b
+            total_time = parse_time(b)
+            print "finish calculation, write to table"
             table.add(time=total_time, cpu=cpu, memory=memory)
 
     table.pareto()
-    table.generate("Performance\ Table of "+command)
+    table.generate("PerformanceTable of "+command)
