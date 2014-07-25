6d75647c0d4bdcdc6375d93f0e0ee2ad700319ed
Liang Shang <liang.shang13@imperial.ac.uk>
2014-07-25 16:52:27 +0100

    time changed to real time

--- learning/parse.py
+++ learning/parse.py
@@ -13,9 +13,8 @@ if __name__ == "__main__":
         profile = line.split(" ")
         cpu = profile[0]
         memory = profile[1]
-        sys_time_str = profile[3]
-        user_time_str = profile[5]
-        time = parse_m_s(sys_time_str) + parse_m_s(user_time_str)
+        real_time_str = profile[3]
+        time = parse_m_s(real_time_str)
         previous_time = performance_table.get(cpu, memory)
         if previous_time:
             performance_table.add(previous_time+time, cpu, memory)
