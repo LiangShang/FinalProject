15ade97205c8418723f91676d54c3e7d9b28d7e7
Liang Shang <liang.shang13@imperial.ac.uk>
2014-08-04 21:11:37 +0100

    move application to application dir

--- learning/learning.sh
+++ learning/learning.sh
@@ -8,8 +8,9 @@ command=$1
 matrix_size=$2
 image="ubuntu_python"
 echo "Creating random matrix for test"
-python matrix.py $matrix_size
+cd ../application;python matrix.py $matrix_size
 echo "Matrix generated"
+cd ../learning/
 
 for i in {1};
 do
@@ -21,11 +22,16 @@ do
         # put process number to script
         process=$cpu
         echo calculate memory: $memory cpu: $cpu
-        #echo time timeout 1m $command $matrix_size $process >script
-        echo time $command $matrix_size $process >script
+        #echo time timeout 1m $command $matrix_size $process >../application/script
+        echo time $command $matrix_size $process > ../application/script
+
+        cd ../application
         #(docker run -i -v `pwd`:/Final --rm -m $memory --cpuset=0-$(($cpu-1)) -w /Final  $image bash script) 2> tmp
+
         (bash script)  2> tmp #for host running
+
         real_time_str=`cat tmp| head -n 2| tail -n 1`
+        cd ../learning
 
         echo $cpu $memory $real_time_str >> statistics
 
@@ -38,8 +44,8 @@ done;
 echo "learning is finished"
 python parse.py "$command $matrix_size"
 
-rm -f tmp
-rm -f script
+rm -f ../application/tmp
+rm -f ../application/script
 rm -f statistics
 
 
