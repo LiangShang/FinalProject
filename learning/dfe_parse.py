97910982351cb8acfc469f522109488493b3b882
ls1312 <liang.shang13@imperial.ac.uk>
2014-08-06 19:14:43 +0100

    add dfe_learning and parsing scripts

---
+++ learning/dfe_parse.py
@@ -0,0 +1,32 @@
+import sys
+from exe_time import parse_m_s
+
+__author__ = 'Sherlock'
+import table
+
+if __name__ == "__main__":
+
+    performance_table = table.PerformanceTable()
+    f = open("statistics", "r")
+
+    for line in f:
+        profile = line.split(" ")
+        if len(profile) > 5:
+            continue
+        cpu = profile[0]
+        memory = profile[1]
+        real_time_str = profile[4]
+        time = float(real_time_str)/1000
+        previous_time = performance_table.get(cpu, memory)
+        if previous_time:
+            performance_table.add(previous_time+time, cpu, memory)
+        else:
+            performance_table.add(time, cpu, memory)
+    performance_table.average()
+    performance_table.pareto()
+    command_names = sys.argv
+    command_names[1] = command_names[1].replace('./', '')
+    command_name = ""
+    for i in range(1, len(command_names)):
+        command_name += command_names[i]+" "
+    performance_table.generate("performance of "+command_name)
